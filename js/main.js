60313373782fae193a8f64ee47eafc5345be8140
Kenny Jackelen <kenny@kennyjackelen.com>
2013-08-10 11:01:02 -0700

    flight paths work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var gm = google.maps;
 var map;
+var collections = {};
+var citiesBuilt = false;
+var flightsBuilt = false;
+var ballparksBuilt = false;
 
-var initialize = function initialize() {
+var initializeMap = function initializeMap() {
+  google.maps.visualRefresh = true;
   var mapOptions = {
     zoom: 4,
     center: new gm.LatLng(39, -92),
@@ -10,5 +15,114 @@ var initialize = function initialize() {
   var mapElement = $('#map').get(0);
   map = new gm.Map(mapElement, mapOptions);
 };
+
+var toggleCities = function toggleCities() {
+  var active = $('#cities_toggle').hasClass('active');
+  if ( !active ) {
+    showCityMarkers();
+  } else {
+    hideCityMarkers();
+  }
+};
+
+var toggleFlights = function toggleFlights() {
+  var active = $('#flights_toggle').hasClass('active');
+  if ( !active ) {
+    showFlightPaths();
+  } else {
+    hideFlightPaths();
+  }
+};
+
+var toggleBallparks = function toggleBallparks() {
+
+};
+
+var bindFilters = function bindFilters() {
+  $('#cities_toggle').click(toggleCities);
+  $('#flights_toggle').click(toggleFlights);
+  $('#ballparks_toggle').click(toggleBallparks);
+};
+
+var initialize = function initialize() {
+  initializeMap();
+  bindFilters();
+};
+
+var buildCityMarkers = function buildCityMarkers() {
+
+};
+
+var showCityMarkers = function showCityMarkers() {
+
+};
+
+var hideCityMarkers = function hideCityMarkers() {
+
+};
+
+var buildFlightPaths = function buildFlightPaths() {
+  if ( flightsBuilt ) {
+    return;
+  }
+
+  collections.flights = {};
+
+  for ( var flight in myFlights) {
+    var first, second;
+    var src = myFlights[flight].src;
+    var dest = myFlights[flight].dest;
+    if ( src < dest ) {
+      first = src;
+      second = dest;
+    } else {
+      first = dest;
+      second = src;
+    }
+    var key = first + second;
+    if ( collections.flights[key] === undefined ) {
+      var coordinates = [ iata[first], iata[second] ];
+      var flightPath = new gm.Polyline({
+        path: coordinates,
+        strokeColor: '#003366',
+        geodesic: true,
+        strokeOpacity: 0.75,
+        strokeWeight: 1
+      });
+      collections.flights[key] = flightPath;
+    } else {
+      collections.flights[key].strokeWeight++;
+    }
+  }
+
+  flightsBuilt = true;
+};
+
+var showFlightPaths = function showFlightPaths() {
+  buildFlightPaths();
+  for ( var path in collections.flights ) {
+    collections.flights[path].setMap(map);
+  }
+};
+
+var hideFlightPaths = function hideFlightPaths() {
+  buildFlightPaths();
+  for ( var path in collections.flights ) {
+    collections.flights[path].setMap(null);
+  }
+};
+
+var buildBallparkMarkers = function buildBallparkMarkers() {
+
+};
+
+var showBallparkMarkers = function showBallparkMarkers() {
+
+};
+
+var hideBallparkMarkers = function hideBallparkMarkers() {
+
+};
+
 
 $(document).ready(initialize);
---
+++ js/myFlights.js
@@ -0,0 +1,159 @@
+var myFlights = [
+  // December 1998 - Christmas in Arizona
+  {src: 'MSP', dest: 'DFW'},
+  {src: 'DFW', dest: 'PHX'},
+  {src: 'PHX', dest: 'DFW'},
+  {src: 'DFW', dest: 'MSP'},
+  // February 2005 - Trip #1 to Xavier
+  {src: 'MSP', dest: 'CVG'},
+  {src: 'CVG', dest: 'MSP'},
+  // March 2005 - Trip #3 to Xavier
+  {src: 'MSP', dest: 'CVG'},
+  {src: 'CVG', dest: 'MSP'},
+  // July 2005 - Graduation Trip with Dad
+  {src: 'MSP', dest: 'ORD'},
+  {src: 'ORD', dest: 'SNA'},
+  {src: 'SNA', dest: 'ORD'},
+  {src: 'ORD', dest: 'MSP'},
+  // November 2005 - Back to School from Thanksgiving
+  {src: 'MSP', dest: 'ORD'},
+  // March 2007 - Home for Dad's 50th Birthday
+  {src: 'SBN', dest: 'MSP'},
+  {src: 'MSP', dest: 'SBN'},
+  // July 2008 - Vacation in Washington, DC
+  {src: 'MSP', dest: 'IAD'},
+  {src: 'IAD', dest: 'MSP'},
+  // October 2008 - Job Interview in Colorado Springs
+  {src: 'SBN', dest: 'ORD'},
+  {src: 'ORD', dest: 'COS'},
+  {src: 'COS', dest: 'ORD'},
+  {src: 'ORD', dest: 'SBN'},
+  // October 2008 - Job Interview in New York City
+  {src: 'MSP', dest: 'EWR'},
+  {src: 'EWR', dest: 'MSP'},
+  // March 2009 - Spring Break in Puerto Rico
+  {src: 'ORD', dest: 'JFK'},
+  {src: 'JFK', dest: 'SJU'},
+  {src: 'SJU', dest: 'JFK'},
+  {src: 'JFK', dest: 'ORD'},
+  // October 2009 - Business Trip to Cleveland
+  {src: 'MSN', dest: 'ORD'},
+  {src: 'ORD', dest: 'CLE'},
+  {src: 'CLE', dest: 'ORD'},
+  {src: 'ORD', dest: 'MSN'},
+  // November 2009 - Business Trip to Fresno
+  {src: 'MSN', dest: 'DEN'},
+  {src: 'DEN', dest: 'FAT'},
+  {src: 'FAT', dest: 'DEN'},
+  {src: 'DEN', dest: 'MSN'},
+  // December 2009 - Business Trip to Annapolis
+  {src: 'MSN', dest: 'DTW'},
+  {src: 'DTW', dest: 'BWI'},
+  {src: 'BWI', dest: 'MSP'},
+  {src: 'MSP', dest: 'MSN'},
+  // January 2010 - Business Trip to Cincinnati
+  {src: 'MSN', dest: 'DTW'},
+  {src: 'DTW', dest: 'CVG'},
+  {src: 'CVG', dest: 'DTW'},
+  {src: 'DTW', dest: 'MSN'},
+  // January 2010 - Business Trip to Fresno
+  {src: 'MSN', dest: 'DEN'},
+  {src: 'DEN', dest: 'FAT'},
+  {src: 'FAT', dest: 'DEN'},
+  {src: 'DEN', dest: 'MSN'},
+  // May-June 2010 - Vacation in Seattle
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'SEA'},
+  {src: 'SEA', dest: 'MSP'},
+  {src: 'MSP', dest: 'MSN'},
+  // June 2010 - Vacation in New Jersey
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'PHL'},
+  {src: 'PHL', dest: 'MKE'},
+  {src: 'MKE', dest: 'MSN'},
+  // August 2010 - Vacation in NY, CT, MA
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'LGA'},
+  {src: 'BOS', dest: 'MKE'},
+  {src: 'MKE', dest: 'MSN'},
+  // January 2011 - Business Trip to Seattle
+  {src: 'MSN', dest: 'MSP'},
+  {src: 'MSP', dest: 'SEA'},
+  {src: 'SEA', dest: 'DTW'},
+  {src: 'DTW', dest: 'MSN'},
+  // February 2011 - Business Trip to Portland
+  {src: 'MSN', dest: 'DEN'},
+  {src: 'DEN', dest: 'PDX'},
+  {src: 'PDX', dest: 'ORD'},
+  {src: 'ORD', dest: 'MSN'},
+  // April 2011 - Vacation in Washington, DC
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'DCA'},
+  {src: 'DCA', dest: 'MKE'},
+  {src: 'MKE', dest: 'MSN'},
+  // April 2011 - Business Trip to Cleveland
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'CLE'},
+  {src: 'CLE', dest: 'MKE'},
+  {src: 'MKE', dest: 'MSN'},
+  // April-May 2011 - Business Trip to Brainerd
+  {src: 'MSN', dest: 'MSP'},
+  {src: 'MSP', dest: 'BRD'},
+  {src: 'BRD', dest: 'MSP'},
+  {src: 'MSP', dest: 'MSN'},
+  // July 2011 - Vacation in San Francisco
+  {src: 'MSN', dest: 'MSP'},
+  {src: 'MSP', dest: 'SFO'},
+  {src: 'SFO', dest: 'CVG'},
+  {src: 'CVG', dest: 'MSN'},
+  // September 2011 - Business Trip and Vacation in Denver
+  {src: 'MSN', dest: 'MKE'},
+  {src: 'MKE', dest: 'DEN'},
+  {src: 'DEN', dest: 'MSN'},
+  // November 2011 - Business Trip and Vacation in Richland and Seattle
+  {src: 'MSN', dest: 'DEN'},
+  {src: 'DEN', dest: 'PSC'},
+  {src: 'PSC', dest: 'SEA'},
+  {src: 'SEA', dest: 'DEN'},
+  {src: 'DEN', dest: 'MSN'},
+  // January 2012 - Business Trip to New Haven
+  {src: 'MSN', dest: 'ORD'},
+  {src: 'ORD', dest: 'BDL'},
+  {src: 'BDL', dest: 'MSP'},
+  {src: 'MSP', dest: 'MSN'},
+  // February 2012 - Vacation in Orlando
+  {src: 'MSN', dest: 'MCO'},
+  {src: 'MCO', dest: 'MSN'},
+  // April 2012 - Business Trip to Cincinnati
+  {src: 'MSN', dest: 'DTW'},
+  {src: 'DTW', dest: 'CVG'},
+  {src: 'CVG', dest: 'ORD'},
+  // June 2012 - Business Trip to San Francisco
+  {src: 'MSN', dest: 'MSP'},
+  {src: 'MSP', dest: 'SFO'},
+  {src: 'SFO', dest: 'MSP'},
+  {src: 'MSP', dest: 'MSN'},
+  // July 2012 - Vacation in College Station
+  {src: 'MSN', dest: 'DFW'},
+  {src: 'DFW', dest: 'CLL'},
+  {src: 'CLL', dest: 'DFW'},
+  {src: 'DFW', dest: 'MSN'},
+  // November 2012 - Vacation in Hawaii
+  {src: 'ORD', dest: 'HNL'},
+  {src: 'HNL', dest: 'LAX'},
+  {src: 'LAX', dest: 'ORD'},
+  {src: 'ORD', dest: 'MSP'},
+  // December 2012 - Business Trip to St. Louis
+  {src: 'MSN', dest: 'ORD'},
+  {src: 'ORD', dest: 'STL'},
+  {src: 'STL', dest: 'ORD'},
+  {src: 'ORD', dest: 'MSN'},
+  // April 2013 - Vacation in College Station
+  {src: 'MSN', dest: 'DFW'},
+  {src: 'DFW', dest: 'CLL'},
+  {src: 'CLL', dest: 'DFW'},
+  {src: 'DFW', dest: 'MSN'},
+  // July-August 2013 - Business Trip to Atlanta
+  {src: 'MSN', dest: 'ATL'},
+  {src: 'ATL', dest: 'MSN'}
+];
