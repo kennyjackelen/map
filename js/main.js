a3158b5d54718ad5cae08fd755c2a2573311d6b3
Kenny Jackelen <kenny@kennyjackelen.com>
2013-08-10 09:05:56 -0700

    initialize map element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+var gm = google.maps;
+var map;
 
+var initialize = function initialize() {
+  var mapOptions = {
+    zoom: 4,
+    center: new gm.LatLng(39, -92),
+    mapTypeId: gm.MapTypeId.ROADMAP
+  };
+  var mapElement = $('#map').get(0);
+  map = new gm.Map(mapElement, mapOptions);
+};
+
+$(document).ready(initialize);
